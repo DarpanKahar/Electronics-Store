c73030353bdb92f7855cc4a14397c62edec5b3df
DarpanKahar <66938132+DarpanKahar@users.noreply.github.com>
2020-07-18 13:16:53 +0530

    Electronics-Store

---
+++ core/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from .views import (CheckoutView,
+                    HomeView,
+                    ItemDetailView,
+                    add_to_cart,
+                    remove_from_cart,
+                    OrderSummaryView,
+                    remove_single_item_from_cart,
+                    PaymentView
+                    )
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', HomeView.as_view(),name='item-list'),
+    path('checkout/', CheckoutView.as_view(),name='checkout'),
+    path('order-summary/', OrderSummaryView.as_view(),name='order-summary'),
+    path('product/<slug>/', ItemDetailView.as_view(),name='product'),
+    path('add_to_cart/<slug>/', add_to_cart,name='add_to_cart'),
+    path('remove_from_cart/<slug>/', remove_from_cart,name='remove_from_cart'),
+    path('remove_item_from_cart/<slug>/', remove_single_item_from_cart,name='remove_single_item_from_cart'),
+    path('payment/<payment_option>/', PaymentView.as_view(),name='payment'),
+
+]
